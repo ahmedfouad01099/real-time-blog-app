7c84e309b52e0c19ae437cfa3678e330347c1cd5
Ahmed Fouad <ahmedfouad01099@gmail.com>
2020-12-18 17:20:12 +0200

    Updates

---
+++ precache-manifest.c7222f266eaa94b8d0cfe9652e26c30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e09c9ff0941d3f786c1",
+    "url": "/real-time-blog-app/static/css/main.e4ddfdcd.chunk.css"
+  },
+  {
+    "revision": "4e09c9ff0941d3f786c1",
+    "url": "/real-time-blog-app/static/js/main.4e09c9ff.chunk.js"
+  },
+  {
+    "revision": "cdd0c1f5e23d99b5a756",
+    "url": "/real-time-blog-app/static/js/1.cdd0c1f5.chunk.js"
+  },
+  {
+    "revision": "aaf6a7070ba651049991",
+    "url": "/real-time-blog-app/static/js/runtime~main.aaf6a707.js"
+  },
+  {
+    "revision": "5f9c42074ccda4691a6a1841d972ebe8",
+    "url": "/real-time-blog-app/index.html"
+  }
+];
